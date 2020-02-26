b72f99085e11a1c0582394f8725721227c013e37
Antonykirunyu <kirunyuanthony@gmail.com>
2020-02-26 11:23:31 +0300

    final commit

---
+++ script.js
@@ -0,0 +1,170 @@
+var saySomething = function(whatToSay) {
+    alert(whatToSay);
+    };
+    var add = function(number1, number2) {
+    return number1 + number2;
+    };
+    saySomething ("hi");
+    add(3,5);
+    var result=add(3,5);
+    alert(result);
+    var outputText = "The sum is " + result + ".";
+    saySomething(outputText);
+    var add =function(number1, number2) {
+    return number1+ number2;
+    }
+    add(4,5);
+    var result=add(4,5);
+    alert(result);var saySomething = function(whatToSay) {
+      alert(whatToSay);
+      };
+      var add = function(number1, number2) {
+      return number1 + number2;
+      };
+      saySomething ("hi");
+      add(3,5);
+      var result=add(3,5);
+      alert(result);
+      var outputText = "The sum is " + result + ".";
+      saySomething(outputText);
+      var add =function(number1, number2) {
+      return number1+ number2;
+      }
+      add(4,5);
+      var result=add(4,5);
+      alert(result);
+      var greetings ="hello Loisa!";
+      alert(greetings);
+      
+      var subtract =function (number1,number2) {
+      return number1-number2;
+      }
+      subtract(9,2);
+      var result=subtract(9,2);
+      alert(result);
+      var multiply=function(number1,number2) {
+      return number1*number2;
+      }
+      multiply(4,5);
+      var result=multiply(4,5);
+      alert(result);
+      var threeTimes =function (number1, number2, number3) {
+      return number1*number2*number3;
+      }
+      threeTimes(2,2,2);
+      var result=threeTimes(2,2,2);
+      alert(result);
+      var divide =function (number1,number2) {
+      return number1/number2;
+      }
+      divide(9,2);
+      var result =divide(9,2);
+      alert(result);
+      var remainder =function (number1,number2) {
+      return number1%number2;
+      }
+      remainder(9,2);
+      var result=remainder(9,2);
+      alert(result);
+      var age =prompt("How old are you?");
+      var name=prompt("What is your name?");
+      var food =prompt("What is your favourite food?");
+      var combine= age+ ' ' +name + ' ' +food;
+      alert(combine);
+      var add = function(number1, number2) {
+        return number1 + number2;
+      };
+      var number1 = parseInt(prompt("Enter a number:"));
+      var number2 = parseInt(prompt("Enter another number:"));
+      alert(add(number1, number2));
+      
+      var bmi =function(number1,number2) {
+        return number1/(number2*number2);
+      };
+    var number1=parseInt(prompt("Enter your mass in kg"));
+    var number2 =parseFloat(prompt("Enter your height in metres"));
+    bmi(number1,number2)
+    var result=bmi(number1,number2);
+    var outputText = "Your BMI is " +result +"."
+    alert(outputText);
+  
+    var greetings ="hello Loisa!";
+    alert(greetings);
+    
+    var subtract =function (number1,number2) {
+    return number1-number2;
+    }
+    subtract(9,2);
+    var result=subtract(9,2);
+    alert(result);
+    var multiply=function(number1,number2) {
+    return number1*number2;
+    }
+    multiply(4,5);
+    var result=multiply(4,5);
+    alert(result);
+    var threeTimes =function (number1, number2, number3) {
+    return number1*number2*number3;
+    }
+    threeTimes(2,2,2);
+    var result=threeTimes(2,2,2);
+    alert(result);
+    var divide =function (number1,number2) {
+    return number1/number2;
+    }
+    divide(9,2);
+    var result =divide(9,2);
+    alert(result);
+    var remainder =function (number1,number2) {
+    return number1%number2;
+    }
+    remainder(9,2);
+    var result=remainder(9,2);
+    alert(result);
+    var age =prompt("How old are you?");
+    var name=prompt("What is your name?");
+    var food =prompt("What is your favourite food?");
+    var combine= age+ ' ' +name + ' ' +food;
+    alert(combine);
+    var add = function(number1, number2) {
+      return number1 + number2;
+    };
+    var number1 = parseInt(prompt("Enter a number:"));
+    var number2 = parseInt(prompt("Enter another number:"));
+    alert(add(number1, number2));
+    
+    var bmi =function(number1,number2) {
+      return number1/(number2*number2);
+    };
+  var number1=parseInt(prompt("Enter your mass in kg"));
+  var number2 =parseFloat(prompt("Enter your height in metres"));
+  bmi(number1,number2)
+  var result=bmi(number1,number2);
+  var outputText = "Your BMI is " +result +"."
+  alert(outputText);
+
+  var x =prompt("Enter a number") ;
+    if (x% 2===0) {
+      alert("Even number");
+    }
+    else {
+      alert("Odd Number");
+    }
+    var y =prompt ("Enter a number");
+    if (y % 3===0) {
+      alert ("ping");
+    }
+    else if (y %5===0) {
+      alert("pong");
+    }
+    else if (y %3===0 || y % 5===0);
+    alert ("ping")
+  var x =prompt ("Enter a number"); {
+      if (x.length < 4 || x.length > 4) {
+        alert ("Enter a valid year");
+      }else if(x % 4 ===0 || x % 400 ===0) {
+        alert ("leap year");
+      }else {
+        alert ("Not a leap year");
+      }
+    }
